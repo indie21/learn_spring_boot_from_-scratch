442ad1dbc7a09a374fbf6a79b29905ff344fddaf
zhuoyikang <zhuoyikang@gmail.com>
2016-10-08 21:08:35 +0800

    使用FastJson来序列化

--- src/main/java/com/example/DemoController.java
+++ src/main/java/com/example/DemoController.java
@@ -1,6 +1,9 @@
 package com.example;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import com.alibaba.fastjson.JSONObject;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
@@ -11,8 +14,18 @@ public class DemoController {
     public Demo hello() {
         Demo demo = new Demo();
         demo.setId(1);
-        demo.setName("hello world 2");
+        demo.setName("hello world 23");
         return demo;
     }
+
+
+    @RequestMapping("/getJson")
+    public String json() {
+        Demo demo = new Demo();
+        demo.setId(1);
+        demo.setName("hello world 23");
+        return JSONObject.toJSONString(demo);
+    }
+
 
 }
