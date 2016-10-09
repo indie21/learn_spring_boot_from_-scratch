f859fc4e85ee242c6d317b663d9f53b7a38f0c69
zhuoyikang <zhuoyikang@gmail.com>
2016-10-09 10:07:28 +0800

    自定义静态文件路径

---
+++ src/main/java/com/example/MyWebAppConfigurer.java
@@ -0,0 +1,22 @@
+package com.example;
+
+//import org.springboot.sample.interceptor.MyInterceptor1;
+//import org.springboot.sample.interceptor.MyInterceptor2;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+
+@Configuration
+public class MyWebAppConfigurer extends WebMvcConfigurerAdapter {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/myres/**").addResourceLocations("classpath:/myres/");
+        super.addResourceHandlers(registry);
+    }
+
+}
+
+
